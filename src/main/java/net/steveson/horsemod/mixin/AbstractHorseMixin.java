df49f6436bfcca1ce05542b5786cf3a7818bfdf0
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-03-12 19:37:51 -0400

    breeding code finished

--- src/main/java/net/steveson/horsemod/mixin/AbstractHorseMixin.java
+++ src/main/java/net/steveson/horsemod/mixin/AbstractHorseMixin.java
@@ -1,15 +1,22 @@
 package net.steveson.horsemod.mixin;
 
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NonTameRandomTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.steveson.horsemod.Config;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -21,7 +28,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.function.Predicate;
 
 @Mixin(AbstractHorse.class)
-public abstract class AbstractHorseMixin {
+public abstract class AbstractHorseMixin extends Animal{
+
+    protected AbstractHorseMixin(EntityType<? extends Animal> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
 
     /**
      * @author
@@ -45,33 +56,37 @@ public abstract class AbstractHorseMixin {
         if (pMax <= pMin || cfgMax <= cfgMin) {
             throw new IllegalArgumentException("Incorrect range for an attribute");
         } else {
-            if (pValue1 == pValue2){
-                return pValue1;
+            double parentValue1 = Mth.clamp(pValue1, cfgMin, cfgMax);
+            double parentValue2 = Mth.clamp(pValue2, cfgMin, cfgMax);
+            if (Math.abs(parentValue1 - parentValue2) <= 0.0025 * (pMax-pMin)){
+                double avr = (cfgMax + cfgMin) / 2;
+                if (Math.abs(avr - parentValue1) >= Math.abs(avr - parentValue2)) {
+                    return parentValue1;
+                } else {
+                    return parentValue2;
+                }
             } else {
+                double d0partOfRange = 0.3 * (pMax - pMin);
+                double d1maxSpread = Math.abs(parentValue1 - parentValue2) + d0partOfRange;
+                double d2parentalAverage = (parentValue1 + parentValue2) / 2;
+                double d3randomness = (pRandom.nextDouble() + pRandom.nextDouble() + pRandom.nextDouble()) / 3.0D - 0.5D;
+                double d4unclampedResult = d2parentalAverage + d1maxSpread * d3randomness;
+
+                return Mth.clamp(d4unclampedResult, cfgMin, cfgMax);
 
                 //test code only
-                return cfgMax;
+//                return cfgMax;
             }
         }
     }
 
-//    @Inject(method = "registerGoals", at = @At(value = "HEAD"))
-//    protected void registerGoals(CallbackInfo ci) {
-//
-//        this.goalSelector.addGoal(3, new NonTameRandomTargetGoal<>(this, Animal.class, false, PREY_SELECTOR));
-//
-//
-//    }
-//
-//
-//    @Unique
-//    //used to be public
-//    private static final Predicate<LivingEntity> PREY_SELECTOR = (p_289448_) -> {
-//        EntityType<?> entitytype = p_289448_.getType();
-//        return entitytype == EntityType.CHICKEN
-////                && p_289448_.isBaby()
-//                ;
-//    };
-
-
+    /**
+     * @author
+     * @reason fall damage reduction for ultra-high jumpers
+     */
+    @Overwrite
+    // this also uses "extends Animal"
+    protected int calculateFallDamage(float pDistance, float pDamageMultiplier) {
+        return Mth.ceil((pDistance * 0.5F - 3.0F - getAttributeValue(Attributes.JUMP_STRENGTH) * 3) * pDamageMultiplier);
+    }
 }
