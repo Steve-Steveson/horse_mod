048d0f0a46ac4b66fb80895ca976d1eba8f0f2b7
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-03-12 20:59:59 -0400

    config improved and healing

--- src/main/java/net/steveson/horsemod/Config.java
+++ src/main/java/net/steveson/horsemod/Config.java
@@ -19,76 +19,59 @@ public class Config
 {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-//    private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-//            .comment("Whether to log the dirt block on common setup")
-//            .define("logDirtBlock", true);
-
     // 15 - 30
     private static final ForgeConfigSpec.DoubleValue H_MAX_HEALTH = BUILDER
-            .comment("Horse Max Health")
-            .defineInRange("maxHealth", 40, 0, Double.MAX_VALUE);
+            .comment("Highest possible Health value attainable when breeding Horses")
+            .comment("Vanilla value: 30")
+            .defineInRange("maxHealth", 30, 0, 1024.0);
     private static final ForgeConfigSpec.DoubleValue H_MIN_HEALTH = BUILDER
-            .comment("Horse Min Health")
-            .defineInRange("minHealth", 10, 0, Double.MAX_VALUE);
+            .comment("Lowest possible Health value attainable when breeding Horses")
+            .comment("Vanilla value: 15")
+            .defineInRange("minHealth", 15, 0, 1024.0);
 
     // 0.4 - 1
     private static final ForgeConfigSpec.DoubleValue H_MAX_JUMP = BUILDER
-            .comment("Horse Max Jump Strength")
-            .defineInRange("maxJump", 1.5, 0, Double.MAX_VALUE);
+            .comment("Highest possible Jump Strength attainable when breeding Horses")
+            .comment("Vanilla value: 1.0")
+            .defineInRange("maxJump", 1.0, 0, 255);
     private static final ForgeConfigSpec.DoubleValue H_MIN_JUMP = BUILDER
-            .comment("Horse Min Jump Strength")
-            .defineInRange("minJump", 0.2, 0, Double.MAX_VALUE);
+            .comment("Lowest possible Jump Strength attainable when breeding Horses")
+            .comment("Vanilla value: 0.4")
+            .defineInRange("minJump", 0.4, 0, 255);
 
     // 0.1125 - 0.3375
     private static final ForgeConfigSpec.DoubleValue H_MAX_SPEED = BUILDER
-            .comment("Horse Max Speed")
-            .defineInRange("maxSpeed", 0.5, 0, Double.MAX_VALUE);
+            .comment("Highest possible Speed value attainable when breeding Horses")
+            .comment("Vanilla value: 0.3375")
+            .defineInRange("maxSpeed", 0.3375, 0, 100);
     private static final ForgeConfigSpec.DoubleValue H_MIN_SPEED = BUILDER
-            .comment("Horse Min Speed")
-            .defineInRange("minSpeed", 0.05, 0, Double.MAX_VALUE);
+            .comment("Lowest possible Speed value attainable when breeding Horses")
+            .comment("Vanilla value: 0.1125")
+            .defineInRange("minSpeed", 0.1125, 0, 100);
 
-//    public static final ForgeConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-//            .comment("What you want the introduction message to be for the magic number")
-//            .define("magicNumberIntroduction", "The magic number is... ");
-
-    // a list of strings that are treated as resource locations for items
-    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+    private static final ForgeConfigSpec.BooleanValue H_EATS_CHICKENS = BUILDER
+            .comment("Whether horses can have a little snack as a treat")
+            .define("getHungry", true);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
-//    public static boolean logDirtBlock;
+    public static boolean eatChicken;
     public static double maxHealth;
     public static double minHealth;
     public static double maxJump;
     public static double minJump;
     public static double maxSpeed;
     public static double minSpeed;
-//    public static String magicNumberIntroduction;
-    public static Set<Item> items;
-
-    private static boolean validateItemName(final Object obj)
-    {
-        return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
-    }
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
     {
-//        logDirtBlock = LOG_DIRT_BLOCK.get();
+        eatChicken = H_EATS_CHICKENS.get();
         maxHealth = H_MAX_HEALTH.get();
         minHealth = H_MIN_HEALTH.get();
         maxJump = H_MAX_JUMP.get();
         minJump = H_MIN_JUMP.get();
         maxSpeed = H_MAX_SPEED.get();
         minSpeed = H_MIN_SPEED.get();
-
-//        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
-
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
-                .collect(Collectors.toSet());
     }
 }
--- src/main/java/net/steveson/horsemod/mixin/HorseMixin.java
+++ src/main/java/net/steveson/horsemod/mixin/HorseMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.level.Level;
+import net.steveson.horsemod.Config;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
@@ -25,14 +26,18 @@ public class HorseMixin extends AbstractHorse{
     @Unique
     protected void addBehaviourGoals() {
         super.addBehaviourGoals();
-        this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
-        this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, Chicken.class, true, PREY_SELECTOR));
-
+        if(Config.eatChicken) {
+            this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
+            this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, Chicken.class, true, PREY_SELECTOR));
+        }
     }
 
     @Unique
     public boolean doHurtTarget(Entity pEntity) {
         this.playSound(SoundEvents.HORSE_EAT, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+        if (this.getHealth() < this.getMaxHealth()) {
+            this.heal(2);
+        }
         return pEntity.hurt(this.damageSources().mobAttack(this), 4.0F);
     }
 
