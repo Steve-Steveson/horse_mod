bbb3e2bf9bb505ba095bcbbc65d3e9324d872f95
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-07-01 22:52:07 -0400

    made exception throw more specific

--- src/main/java/net/steveson/horsemod/mixin/AbstractHorseMixin.java
+++ src/main/java/net/steveson/horsemod/mixin/AbstractHorseMixin.java
@@ -54,11 +54,11 @@ public abstract class AbstractHorseMixin extends Animal{
         }
 //        System.out.println(pMax + " became " + cfgMax);
         if (pMax <= pMin || cfgMax < cfgMin) {
-            throw new IllegalArgumentException("Incorrect range for an attribute");
+            throw new IllegalArgumentException("Incorrect range for a horse attribute");
         } else {
             double parentValue1 = Mth.clamp(pValue1, cfgMin, cfgMax);
             double parentValue2 = Mth.clamp(pValue2, cfgMin, cfgMax);
-            if (Config.keepMatching && Math.abs(parentValue1 - parentValue2) <= 0.0025 * (pMax-pMin)){
+            if (Config.keepMatching && Math.abs(parentValue1 - parentValue2) <= 0.0025 * (pMax-pMin)) {
                 double avr = (cfgMax + cfgMin) / 2;
                 if (Math.abs(avr - parentValue1) >= Math.abs(avr - parentValue2)) {
                     return parentValue1;
